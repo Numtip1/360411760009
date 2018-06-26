4bb8162e80a3a452642cc223a6f9d7bd3ce375a9
Numtip1 <noomthip7674@gmail.com>
2018-06-26 15:07:54 +0700

    Test upload

---
+++ src/lab1/myCode.java
@@ -0,0 +1,15 @@
+package lab1;
+
+public class myCode {
+    //main
+    public static void main(String[] args) {
+
+
+        System.out.println("hello world");
+
+        System.out.println("นางสาวน้ำทิพย์ เขียวจันทร์ ");
+        System.out.println("รหัส 360411760009");
+        System.out.println("MIT421");
+        System.out.println("มหาวิทยาลัยเทคโนโลยีราชมงคลศรีวิชัย วิทยาเขตนครศรีธรรมราช ไสใหญ่");
+    }
+}
