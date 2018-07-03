f5f9ce510fcad1e58db60d22156bfcf4ac723193
maynumtip <noomthip7674@gmail.com>
2018-07-03 13:58:21 +0700

    Test1

--- src/lab1/myCode2.java
+++ src/lab1/myCode2.java
@@ -5,6 +5,7 @@ public class myCode2 {
     //main
     public static void main(String[] args) {
         System.out.println("noomthip7674@gmail.com");
+        System.out.println("May");
 
     }
 }
