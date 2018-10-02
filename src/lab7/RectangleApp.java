b52e6aeaf692f1bc320c20965c1ebe1140e6a3fe
maynumtip <noomthip7674@gmail.com>
2018-10-02 14:30:39 +0700

    lap7

---
+++ src/lab7/RectangleApp.java
@@ -0,0 +1,21 @@
+package lab7;
+
+public class RectangleApp {
+    public  static  void  main ( String []args){
+        //create object
+        Rectangle box1 = new Rectangle();
+
+        box1.setLength (5.0);
+        box1.setWidth(5.0);
+
+        System.out.println(box1.getLength());
+        System.out.println(box1.getwidth());
+
+        Rectangle box2 = new Rectangle (8.0,8.0);
+        System.out.println(box2.getLength());
+        System.out.println(box2.getwidth());
+
+        System.out.println("box1 :" +box1.getArea());
+        System.out.println("box2: "+box2.getArea());
+    }//main
+}//class
